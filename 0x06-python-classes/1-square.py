bb0bc4ee4399750bd968f6eba3e30cd939c634e6
Bacia22 <adolfobacia@yahoo.com>
2023-03-28 12:08:25 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+class Square:
+    """Represent a square."""
+
+    def__init__(self, size):
+        """Initilize a new Square.
+
+        Args:
+        size(int): The size the new square.
+        """
+        sef.__size = size
