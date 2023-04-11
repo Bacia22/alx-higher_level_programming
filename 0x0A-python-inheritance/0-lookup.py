f24db7b903a9d4c6f256a52e7461e5331dd6c12a
Bacia22 <adolfobacia@yahoo.com>
2023-04-11 15:25:14 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+=============================
+Module with the method lookup
+=============================
+"""
+
+def lookup(obj):
+    """Function for return the attributes from an object"""
+
+    return dir(obj)
