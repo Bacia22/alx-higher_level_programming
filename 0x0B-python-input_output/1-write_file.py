e8b06b0879f3ecfd139b469282e28e35ecdbd8b2
Bacia22 <adolfobacia@yahoo.com>
2023-04-12 15:21:34 -0700

    1-write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """write_file module.
-
 Contains a function that writes a text file.
 """
 
 
-def write file(filename="", text=""):
+def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns the numebr of characters written.
+    Writes a string to a text file (UTF8) and
+    returns the number of characters written.
     """
     with open(filename, 'w') as f:
         return f.write(text)
