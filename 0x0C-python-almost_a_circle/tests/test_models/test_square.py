88be0c82fed036db23a81cabb2613ed711edeca9
Bacia22 <adolfobacia@yahoo.com>
2023-04-17 12:33:49 -0700

    test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,7 @@
+Test that checks PEP8
+        """
+        syntax = pep8.StyleGuide(quit=True)
+        check = syntax.check_files(['models/square.py'])
+        self.assertEqual(
+            check.total_errors, 0,
+            "Found code style errors (and warnings)."
