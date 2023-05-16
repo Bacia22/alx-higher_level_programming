7fcdb0b7d6fcd1b3fa52a2fe679002376c5e8e75
Bacia22 <adolfobacia@yahoo.com>
2023-05-16 15:46:25 -0700

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `fisrt_table` (`id`, `name`) VALUES (89, Best School);
+-- Inserts a new row into the table first-table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
