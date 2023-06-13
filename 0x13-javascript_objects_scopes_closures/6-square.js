5613f95bad790ffa4bea600432a6ebe08c3c2b0e
Bacia22 <adolfobacia@yahoo.com>
2023-06-13 16:47:52 -0700

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js')
+{
+	charPrint (c)
+	{
+		if (c === undefined)
+		{
+			this.print();
+		}
+		else
+		{
+			forn(let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+		}
+	}
+};
