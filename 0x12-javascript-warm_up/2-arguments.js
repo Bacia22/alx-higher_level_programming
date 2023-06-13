ee7216383c00645588091ae21e88925b5697bc1b
Bacia22 <adolfobacia@yahoo.com>
2023-06-12 17:17:29 -0700

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (process.argv.length < 3)
+{
+	console,log('No argumet'0;
+}
+else if (process.argv.length === 3)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Argument found');
+}
