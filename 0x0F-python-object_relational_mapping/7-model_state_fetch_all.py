a6244f012552c50e74b8d603d2a252d6325b7ba6
Bacia22 <adolfobacia@yahoo.com>
2023-06-23 04:36:32 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,25 @@
-#!/usr/bin/pyton3
+#!/usr/bin/python3
 """
-Script that lists all State objects from the database - Usage module SQLAlchemy
+Script that lists all State objects from the database - Using module SQLAlchemy
 """
 from model_state import Base, State
 from sys import argv
 from sqlalchemy import create_engine
-from sqllchemy.com import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
-if __name__== "__main":
+if __name__ == "__main__":
 
     # create an engine
-    engine = create_engine('mysql+mmysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3], pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-        # create a configured "Session" class
-        Session = sessionmaker(bind=engine)
-        #create a Session
-        session = Session()
-        Base.metadata.create_all(engine)
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
+    Base.metadata.create_all(engine)
 
-        s_tate = session.query(State).order_by(State.id).all()
-        for state in s_tate:
+    s_tate = session.query(State).order_by(State.id).all()
+    for state in s_tate:
         print("{}: {}".format(state.id, state.name))
-        session.close()
+    session.close()
