1a7bc69e9b974e815c12de2b16ff79b0ec2ee468
Bacia <adolfobacia@yahoo.com>
2023-07-25 15:36:23 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,16 @@
-t request = require('request');
+#!/usr/bin/node
+const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  if (error == null) {
-    const myObj = {};
-    const obj = JSON.parse(body);
-    for (let i = 0; i < obj.length; i++) {
-      if (obj[i].completed === true) {
-        if (myObj[obj[i].userId] === undefined) {
-          myObj[obj[i].userId] = 0;
-        }
-        myObj[obj[i].userId] += 1;
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
-    console.log(myObj);
+    });
+    console.log(completed);
   }
 });
